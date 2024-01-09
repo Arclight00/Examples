cb97d55521e3c80fe40721babc3570c0be292c61
nishantsingh <nishant.singh@mncgroup.com>
2024-01-09 12:37:22 +0530

    added more examples

--- misc.py
+++ misc.py
@@ -24,3 +24,60 @@ with open('saved_dictionary.pkl', 'wb') as f:
 
 with open('saved_dictionary.pkl', 'rb') as f:
     loaded_dict = pickle.load(f)
+
+
+import certifi
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
+
+# Setup plotting
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-whitegrid')
+# Set Matplotlib defaults
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+
+### Example of transforming columns
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.model_selection import GroupShuffleSplit
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import callbacks
+
+spotify = pd.read_csv('../input/dl-course-data/spotify.csv')
+
+X = spotify.copy().dropna()
+y = X.pop('track_popularity')
+artists = X['track_artist']
+
+features_num = ['danceability', 'energy', 'key', 'loudness', 'mode',
+                'speechiness', 'acousticness', 'instrumentalness',
+                'liveness', 'valence', 'tempo', 'duration_ms']
+features_cat = ['playlist_genre']
+
+preprocessor = make_column_transformer(
+    (StandardScaler(), features_num),
+    (OneHotEncoder(), features_cat),
+)
+
+def group_split(X, y, group, train_size=0.75):
+    splitter = GroupShuffleSplit(train_size=train_size)
+    train, test = next(splitter.split(X, y, groups=group))
+    return (X.iloc[train], X.iloc[test], y.iloc[train], y.iloc[test])
+
+X_train, X_valid, y_train, y_valid = group_split(X, y, artists)
+
+X_train = preprocessor.fit_transform(X_train)
+X_valid = preprocessor.transform(X_valid)
+y_train = y_train / 100
+y_valid = y_valid / 100
+
+input_shape = [X_train.shape[1]]
+print("Input shape: {}".format(input_shape))
+
